9ed524b09997ae827c60a3858fcf54dd19d8d78d
mwunnam <mwunnam@gmail.com>
2024-07-31 23:25:18 +0000

    done with task 0

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -4,7 +4,7 @@ import re
 from typing import List
 
 
-def filter_datum(fields: List[str], redaction: str, message: [str],
+def filter_datum(fields: List[str], redaction: str, message: str,
                  separator: str) -> str:
     '''Log message obfuscating '''
     for field in fields:
