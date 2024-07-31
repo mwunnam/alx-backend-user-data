cb572157b6066d6e49810903bcc09c348b721741
mwunnam <mwunnam@gmail.com>
2024-07-31 22:58:56 +0000

    done with task 0

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -5,6 +5,7 @@ from typing import List
 
 def filter_datum(fields: List[str], redaction: str, message: [str],
                 separator: str) -> str:
+    '''Log message obfuscating '''
     pattern = (
         rf"(?<={re.escape(separator)})"
         rf"(?P<field>{'|'.join(map(re.escape, fields))})"
