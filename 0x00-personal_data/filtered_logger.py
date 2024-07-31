891621019be92d89025b31cfa8db47ac375a2cf3
mwunnam <mwunnam@gmail.com>
2024-07-31 22:49:47 +0000

    done with task 0

---
+++ 0x00-personal_data/filtered_logger.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+''' log message obfuscating '''
+import re
+from typing import List
+
+def filter_datum(fields: List[str], redaction: str, message: [str],
+                separator: str) -> str:
+    pattern = (
+        rf"(?<={re.escape(separator)})"
+        rf"(?P<field>{'|'.join(map(re.escape, fields))})"
+        rf"=(?P<value>[^;]*)(?={re.escape(separator)}|$)"
+    )
+    return re.sub(pattern, lambda m: f"{m.group('field')}={redaction}", message)
