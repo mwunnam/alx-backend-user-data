a90facf250c2c6e8ba23880fa5e75c03cd77ea89
mwunnam <mwunnam@gmail.com>
2024-07-31 23:03:25 +0000

    done with task 0

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -3,12 +3,14 @@
 import re
 from typing import List
 
+
 def filter_datum(fields: List[str], redaction: str, message: [str],
-                separator: str) -> str:
+                 separator: str) -> str:
     '''Log message obfuscating '''
     pattern = (
         rf"(?<={re.escape(separator)})"
         rf"(?P<field>{'|'.join(map(re.escape, fields))})"
         rf"=(?P<value>[^;]*)(?={re.escape(separator)}|$)"
     )
-    return re.sub(pattern, lambda m: f"{m.group('field')}={redaction}", message)
+    return re.sub(pattern, lambda m: f"{m.group('field')}={redaction}",
+                  message)
